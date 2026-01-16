c669748a0f96ad44db512ff0036b6c0524159ed0
Rex Tsou <rex@rex-tsou.com>
2026-01-16 15:43:17 +0800

    fix(pkg): remove nuxt module export from client entry to avoid node deps

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,3 @@ export { identifyComponent, extractMetadata } from './core/identifier';
 export { createOverlayController } from './core/overlay';
 export { createVueGrabPlugin } from './plugin';
 export { default } from './plugin';
-export { default as vueGrabNuxtModule } from './nuxt/module';
